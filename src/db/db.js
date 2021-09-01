27716fe10361223093ee7a7d5177e92cd67d4fe4
John <souzajohnlee@gmail.com>
2021-09-01 01:23:28 -0700

    set up db models

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,13 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const { DB_USER, HOST, DATABASE, DB_PASSWORD, DB_PORT } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${HOST}:${DB_PORT}/${DATABASE}`);
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${HOST}:${DB_PORT}/${DATABASE}`
+);
 
 async function testDatabase() {
   try {
@@ -16,4 +18,68 @@ async function testDatabase() {
   }
 }
 
-testDatabase()
+async function synchronizeDatabase() {
+  await sequelize.sync({ force: true });
+  console.log("All models were synchronized successfully.");
+}
+
+const Form = sequelize.define(
+  "Form",
+  {
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {}
+);
+
+const Question = sequelize.define(
+  "Question",
+  {
+    form_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    input_type: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    label: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {}
+);
+
+const Submission = sequelize.define(
+  "Submission",
+  {
+    csv_data: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    form_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {}
+);
+
+// testDatabase();
+// synchronizeDatabase();
+
+module.exports = {
+  sequelize,
+  testDatabase,
+  synchronizeDatabase,
+  Form,
+  Question,
+  Submission,
+};
