d13d6451117024071b3aeacbb669d6c244351d9f
John <souzajohnlee@gmail.com>
2021-09-01 02:01:04 -0700

    create index and post submissions routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-const dotenv = require('dotenv')
-const express = require('express')
+const dotenv = require("dotenv");
+const express = require("express");
+const bodyParser = require("body-parser");
+const { Submission, synchronizeDatabase } = require("./db/db.js");
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
 
-app.get("/forms", (req, res) => {
-  res.json({ hello: "world" });
+app.get("/submissions", async (req, res) => {
+  const submissions = await Submission.findAll();
+  res.json({ submissions: submissions });
+});
+
+app.post("/submissions", async (req, res) => {
+  const csvData = req.body["csv_data"];
+  const submission = await Submission.create({ csv_data: csvData });
+  res.json({ csv_data: submission["csv_data"] });
 });
 
 app.listen(port, () => {
