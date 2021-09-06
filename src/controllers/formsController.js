bce3ce8c3ab64397297664a0e06afb5e812664df
John <souzajohnlee@gmail.com>
2021-09-05 22:56:58 -0700

    add forms index route

--- src/controllers/formsController.js
+++ src/controllers/formsController.js
@@ -2,7 +2,13 @@ const asyncRoute = require("../utils/asyncRoute");
 const Form = require("../models/form.js");
 
 function formsController(app) {
+  app.get("/forms", asyncRoute(index))
   app.post("/forms", asyncRoute(post));
+}
+
+async function index(req, res) {
+  const forms = await Form.findAll()
+  res.json(forms)
 }
 
 async function post(req, res) {
