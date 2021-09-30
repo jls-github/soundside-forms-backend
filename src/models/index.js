b993eb3c8bbda1472943983bb66e5a009488fe90
John <souzajohnlee@gmail.com>
2021-09-30 12:22:23 -0700

    add form input relationship

--- src/models/index.js
+++ src/models/index.js
@@ -8,13 +8,21 @@ const FormInput = require("./formInput");
 
 // Associations
 
+// Submission:Form - 1:M
+
 Submission.belongsTo(Form, { foreignKey: "form_id" });
 Form.hasMany(Submission, { foreignKey: "form_id" });
 
+// Input:Form - M:N
+
 FormInput.belongsTo(Input, { foreignKey: "input_id" });
-Input.hasMany(FormInput, { foreignKey: "input_id" });
 FormInput.belongsTo(Form, { foreignKey: "form_id" });
+Input.hasMany(FormInput, { foreignKey: "input_id" });
 Form.hasMany(FormInput, { foreignKey: "form_id" });
+Form.belongsToMany(Input, {through: FormInput})
+Input.belongsToMany(Form, {through: FormInput})
+
+
 
 const models = { Submission, Form, Question, Input, FormInput };
 
