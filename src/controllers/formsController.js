2c6aebad93ce90e6760b035bb00589f98f332bcc
John <souzajohnlee@gmail.com>
2021-10-01 20:48:11 -0700

    return json without top level key

--- src/controllers/formsController.js
+++ src/controllers/formsController.js
@@ -1,6 +1,10 @@
 const asyncRoute = require("../middleware/asyncRoute");
 const authRoute = require("../middleware/authRoute");
-const {formsPostSerializer, formsIndexSerializer, formsShowSerializer} = require("../serializers/formsSerializers")
+const {
+  formsPostSerializer,
+  formsIndexSerializer,
+  formsShowSerializer,
+} = require("../serializers/formsSerializers");
 const { Form, Input } = require("../models");
 
 function formsController(app) {
@@ -11,22 +15,22 @@ function formsController(app) {
 
 async function index(req, res) {
   const forms = await Form.findAll();
-  const formsJson = formsIndexSerializer(forms)
+  const formsJson = formsIndexSerializer(forms);
   res.json(formsJson);
 }
 
 async function post(req, res) {
   const { guest, name, inputs } = req.body;
-  const form = await Form.createWithInputs(guest, name, inputs)
-  const formJson = await formsPostSerializer(form)
-  res.json({ form: formJson });
+  const form = await Form.createWithInputs(guest, name, inputs);
+  const formJson = await formsPostSerializer(form);
+  res.json(formJson);
 }
 
 async function show(req, res) {
   const { id } = req.params;
   const form = await Form.findByPk(id, { include: [{ model: Input }] });
-  const formJson = formsShowSerializer(form)
-  res.json({ form: formJson });
+  const formJson = formsShowSerializer(form);
+  res.json(formJson);
 }
 
 module.exports = formsController;
